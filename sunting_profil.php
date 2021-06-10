f6a3d941e341721fa4fc7deead0b1ed3dfab1d2f
KevinRizki <kevinrizki94@gmail.com>
2021-06-11 01:29:36 +0700

    beres figma

--- sunting_profil.php
+++ sunting_profil.php
@@ -1,3 +1,14 @@
+<?php
+    include 'config/koneksi.php';
+    $profil = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM user WHERE id_user = '$_SESSION[id_user]'"));
+
+    if(isset($_POST['sunting'])){
+        $sql = mysqli_query($con, "UPDATE user SET nama_lengkap = '$_POST[nama]', username = '$_POST[username]',
+        alamat_email = '$_POST[email]', kota_asal = '$_POST[kota]', tanggal_lahir = '$_POST[tgl]', no_telp = '$_POST[telp]'
+        WHERE id_user = '$_SESSION[id_user]'");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,17 +24,17 @@
     <div class="w-full bg-red-300 fixed flex flex-row">
         <div class="flex flex-row">
             <div class="w-11 bg-red-300 hover:bg-red-400" style="background-image: url('assets/c506cd42d221531be073c743195d1e76.png');background-size: 80%; background-position: center;background-repeat: no-repeat;"></div>
+            <div class="text-white px-6 py-3 ">
+                <a href="beranda.php">Beranda</a>
+            </div>
             <div class="text-white px-6 py-3 border-b-4 border-red-400">
-                <a href="">Beranda</a>
+                <a href="konsultasi.php">Konsultasi</a>
             </div>
             <div class="text-white px-6 py-3">
-                <a href="">Konsultasi</a>
+                <a href="donasi.php">Donasi</a>
             </div>
             <div class="text-white px-6 py-3">
-                <a href="">Donasi</a>
-            </div>
-            <div class="text-white px-6 py-3">
-                <a href="">Transaksi</a>
+                <a href="transaksi.php">Transaksi</a>
             </div>
         </div>
         <div class="text-white px-6 py-3 flex-grow">
@@ -41,14 +52,15 @@
                 </center>
                 <form action="" method="post">
                     <div class="grid grid-cols-1 gap-4">
-                        <input type="text" name="nama" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Nama Lengkapmu">
-                        <input type="text" name="username" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Nama pengguna yang kamu inginkan">
-                        <input type="email" name="username" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Email kamu">
-                        <input type="text" name="kota" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Kota asal kamu">
+                        <input type="text" name="nama" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Nama Lengkapmu" value="<?php echo $profil['nama_lengkap']?>">
+                        <input type="text" name="username" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Nama pengguna yang kamu inginkan" value="<?php echo $profil['username']?>">
+                        <input type="email" name="email" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Email kamu" value="<?php echo $profil['alamat_email']?>">
+                        <input type="text" name="kota" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Kota asal kamu"value="<?php echo $profil['kota_asal']?>">
+                        <input type="text" name="telp" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Kota asal kamu"value="<?php echo $profil['no_telp']?>">
                         <label for="kota" class="text-red-500">Tanggal lahir kamu</label>
-                        <input type="date" name="kota" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Tanggal lahir kamu">
-                        <textarea name="" id="" cols="30" rows="10" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Lorem ipsum"></textarea>
-                        <input type="submit" value="Sunting" class="py-2 px-5 rounded-3xl bg-red-400 text-white font-bold">
+                        <input type="date" name="tgl" class="py-2 px-5 rounded-3xl placeholder-gray-500 bg-red-100 w-96" placeholder="Tanggal lahir kamu" value="<?php echo $profil['tanggal_lahir']?>">
+                        
+                        <input type="submit" name="sunting" value="Sunting" class="py-2 px-5 rounded-3xl bg-red-400 text-white font-bold">
                     </div>
                 </form>
             </div>
